c20700f02a41d8efcf661ed1ce589d4b03037243
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2020-09-16 14:40:38 +0530

    Added code to initialize dirs

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,11 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	homedir "github.com/mitchellh/go-homedir"
+	"github.com/platform9/pf9ctl/pkg/pmk"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -24,10 +26,24 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+
+	if err := initializeBaseDirs(); err != nil {
+		log.Fatalf("Base directory initialization failed: %s\n", err.Error())
 	}
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func initializeBaseDirs() (err error) {
+	log.Println("Received a call to create base dirs")
+
+	err = os.MkdirAll(pmk.Pf9Dir, os.ModeDir)
+	err = os.MkdirAll(pmk.Pf9DBDir, os.ModeDir)
+	err = os.MkdirAll(pmk.Pf9LogDir, os.ModeDir)
+
+	return
 }
 
 func init() {
--- pkg/pmk/constants.go
+++ pkg/pmk/constants.go
@@ -7,12 +7,14 @@ import (
 
 var (
 	homeDir, _ = os.UserHomeDir()
-	pf9Dir     = filepath.Join(homeDir, "pf9")
-	pf9LogDir  = filepath.Join(pf9Dir, "log")
-	pf9DBDir   = filepath.Join(pf9Dir, "db")
-
+	//Pf9Dir is the base pf9dir
+	Pf9Dir = filepath.Join(homeDir, "pf9")
+	//Pf9LogDir is the base path for creating log dir
+	Pf9LogDir = filepath.Join(Pf9Dir, "log")
+	// Pf9DBDir is the base dir for storing pf9 db context
+	Pf9DBDir = filepath.Join(Pf9Dir, "db")
 	// Pf9DBLoc represents location of the context file.
-	Pf9DBLoc = filepath.Join(pf9DBDir, "express.json")
+	Pf9DBLoc = filepath.Join(Pf9DBDir, "express.json")
 	// Pf9Log represents location of the log.
-	Pf9Log = filepath.Join(pf9LogDir, "pf9ctl.log")
+	Pf9Log = filepath.Join(Pf9LogDir, "pf9ctl.log")
 )
