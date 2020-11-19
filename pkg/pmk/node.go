1ffdbc8872fde0e46a6d4af5871a0d7f8b122297
rparikh <2824564+roopakparikh@users.noreply.github.com>
2020-11-19 15:51:54 -0800

    Adding a missing --insecure missing flag in the curl

--- pkg/pmk/node.go
+++ pkg/pmk/node.go
@@ -190,7 +190,7 @@ func installHostAgentLegacy(ctx Context, auth keystone.KeystoneAuth, hostOS stri
 	url := fmt.Sprintf("%s/private/platform9-install-%s.sh", ctx.Fqdn, hostOS)
 	installOptions := fmt.Sprintf("--insecure --project-name=%s 2>&1 | tee -a /tmp/agent_install", auth.ProjectID)
 
-	cmd := fmt.Sprintf(`curl --silent --show-error -H "X-Auth-Token: %s" %s -o /tmp/installer.sh`, auth.Token, url)
+	cmd := fmt.Sprintf(`curl --insecure --silent --show-error -H 'X-Auth-Token:%s' %s -o /tmp/installer.sh`, auth.Token, url)
 	_, err := exec.RunWithStdout("bash", "-c", cmd)
 	if err != nil {
 		return err
