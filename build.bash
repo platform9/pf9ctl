0bd16e63996e8ea08b7bf73aa80f67d0753dc464
“devidas” <devidas@platform9.com>
2021-02-19 17:19:50 +0530

    error fix

--- build.bash
+++ build.bash
@@ -33,5 +33,4 @@ if [ ${TEAMCITY_BUILD_BRANCH} = master ]; then
     ln ${TEAMCITY_CHECKOUT_DIR}/src/github.com/platform9/pf9ctl/cli_setup.sh ${TEAMCITY_CHECKOUT_DIR}/build/publish-to-artf/cli_setup.sh
 else
     echo "Not publishing wrapper script to artifactory since this is not built off master branch."
-
 fi
