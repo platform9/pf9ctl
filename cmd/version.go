26b74dbc149ecd140be0badad1bcf069a26c89c6
JasminAnt <jasmin.dudic@antcolony.io>
2021-10-08 13:41:51 +0200

    Added command to upgrade the cli verison.

--- cmd/version.go
+++ cmd/version.go
@@ -4,11 +4,15 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/platform9/pf9ctl/pkg/util"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
+
+var skipCheck bool
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -22,8 +26,80 @@ var versionCmd = &cobra.Command{
 	},
 }
 
+// versionCmd represents the version command
+var upgrade = &cobra.Command{
+	Use:   "upgrade",
+	Short: "Checks for a new version of the CLI",
+	Long:  "Checks and downloads the new version of the CLI. Use -skipCheck to just download the newest version.",
+	Run:   checkVersion,
+}
+
+func checkVersion(cmd *cobra.Command, args []string) {
+
+	if skipCheck {
+		upgradeVersion()
+		return
+	}
+
+	// Code to compare the current version with the newest version
+	newVersion := getNewestVersion()
+
+	if newVersion != util.Version {
+		upgradeVersion()
+	} else {
+		fmt.Print("You already have the newest version\n")
+	}
+
+}
+
+func getNewestVersion() string {
+	return "pf9ctl version: v1.8"
+}
+
+func upgradeVersion() {
+
+	curlCmd, err := exec.Command("curl", "-sL", "https://pmkft-assets.s3-us-west-1.amazonaws.com/pf9ctl_setup").Output()
+	if err != nil {
+		zap.S().Fatalf("Error downloading the setup ", err)
+	}
+
+	copyCmd := exec.Command("/bin/sh", "-c", "sudo mv /usr/bin/pf9ctl /usr/bin/pf9ctl_backup")
+	err = copyCmd.Run()
+
+	if err != nil {
+		fmt.Println("Error creating backup", err)
+	} else {
+		fmt.Println("Backup successfully created")
+	}
+
+	bashCmd := exec.Command("bash", "-c", string(curlCmd))
+	bashCmd.Stdout = os.Stdout
+	err = bashCmd.Start()
+
+	bashCmd.Wait()
+	if err != nil {
+		fmt.Println("Upgrade failed, reverting to backup")
+		copyCmd := exec.Command("/bin/sh", "-c", "sudo mv /usr/bin/pf9ctl_backup /usr/bin/pf9ctl")
+		err = copyCmd.Run()
+		if err != nil {
+			fmt.Println("Error restoring backup ", err)
+		}
+		zap.S().Fatalf("Error updating pf9ctl. ", err)
+	}
+
+	removeCmd := exec.Command("/bin/sh", "-c", "sudo rm /usr/bin/pf9ctl_backup")
+	err = removeCmd.Run()
+	if err != nil {
+		fmt.Println("Error removing backup ", err)
+	}
+
+}
+
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(upgrade)
+
+	upgrade.Flags().BoolVarP(&skipCheck, "skipCheck", "c", false, "Will skip the verison checks if true")
 
 	// Here you will define your flags and configuration settings.
 
