03ffd5ffa57b7cfe3b2c74dc6c3049b27fa47425
JasminAnt <jasmin.dudic@antcolony.io>
2021-10-19 11:57:42 +0200

    Added command to deauthorize node

---
+++ cmd/detachNode.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+var removeNodeCmd = &cobra.Command{
+	Use:   "remove-node",
+	Short: "decomissions and removes this node",
+	Long:  "Removes the host agent package and decomissions this node from the platform9",
+	Args: func(attachNodeCmd *cobra.Command, args []string) error {
+		if len(args) > 0 {
+			return errors.New("No parameters are needed")
+		}
+		return nil
+	},
+	Run: removeNodeRun,
+}
+
+func init() {
+	rootCmd.AddCommand(removeNodeCmd)
+}
+
+func removeNodeRun(cmd *cobra.Command, args []string) {
+
+	osCheck, err := exec.Command("/bin/sh", "-c", "awk -F= '/^NAME/{print $2}' /etc/os-release").Output()
+	if err != nil {
+		zap.S().Fatalf("Error getting OS version")
+	}
+
+	var command string
+
+	if strings.Contains(string(osCheck), "Ubuntu") {
+		command = "sudo apt-get purge pf9-hostagent"
+
+	} else {
+		command = "sudo yum erase -y pf9-hostagent"
+
+	}
+
+	output := exec.Command("/bin/sh", "-c", command)
+	output.Stdout = os.Stdout
+	output.Stdin = os.Stdin
+	err = output.Start()
+	output.Wait()
+	if err != nil {
+		zap.S().Fatalf("An error has occured ", err)
+	}
+
+	fmt.Println("Removing successfull")
+
+}
