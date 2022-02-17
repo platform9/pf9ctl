beb5e72e7f758a6a6b7fc87291ea9ecd461d7860
devidask27 <devidas@platform9.com>
2022-02-17 17:00:21 +0530

    resolved comments

--- cmd/deauthoriseNode.go
+++ cmd/deauthoriseNode.go
@@ -31,7 +31,7 @@ var deauthNodeCmd = &cobra.Command{
 
 func init() {
 	deauthNodeCmd.Flags().StringVar(&attachconfig.MFA, "mfa", "", "MFA token")
-	deauthNodeCmd.Flags().StringVarP(&ipAdd, "ip", "i", "", "Ip address of the host to be deauthorized")
+	deauthNodeCmd.Flags().StringVarP(&ipAdd, "ip", "i", "", "IP address of the host to be deauthorized")
 	rootCmd.AddCommand(deauthNodeCmd)
 }
 
