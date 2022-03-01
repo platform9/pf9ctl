23d65dbf9c2edf28977fe26b56f87903907072d2
devidask27 <devidas@platform9.com>
2022-03-01 12:38:49 +0530

    pf9-comms dependency pkg added in pre-req checks

--- pkg/platform/debian/debian.go
+++ pkg/platform/debian/debian.go
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	packages                   = []string{"curl", "uuid-runtime", "net-tools"}
+	packages                   = []string{"curl", "uuid-runtime", "net-tools", "logrotate"}
 	packageInstallError        = "Packages not found and could not be installed"
 	MissingPkgsInstalledDebian bool
 	k8sPresentError            = errors.New("A Kubernetes cluster is already running on node")
