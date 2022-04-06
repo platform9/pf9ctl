621ab50cf2238556f6e0201f54b0274a3b711227
devidask27 <devidas@platform9.com>
2022-04-06 16:37:30 +0530

    revert back cloud provider commands

--- cmd/cloudProviders.go
+++ cmd/cloudProviders.go
@@ -16,72 +16,50 @@ import (
 
 var (
 	loadConfig bool
-	kind       string
 )
-
-var checkCmd = &cobra.Command{
-	Use:     "check",
-	Short:   "Checks if the user has Amazon/Azure/Google cloud permissions",
-	Long:    "Checks if service account has the correct roles to use the cloud provider",
-	Example: "pf9ctl provider check --kind=amazon",
-	Run:     checkCmdRun,
-}
 
 var checkGoogleProviderCmd = &cobra.Command{
 	Use:   "check-google-provider",
 	Short: "Checks if the user has Google cloud permissions",
 	Long:  "Checks if service account has the correct roles to use the google cloud provider",
-	//Run:   checkGoogleProviderRun,
+	Run:   checkGoogleProviderRun,
 }
 
 var checkAmazonProviderCmd = &cobra.Command{
 	Use:   "check-amazon-provider",
 	Short: "Checks if the user has Amazon cloud permissions",
 	Long:  "Checks if user has the correct permissions to use the amazon cloud provider",
-	//Run:   checkAmazonProviderRun,
+	Run:   checkAmazonProviderRun,
 }
 
 var checkAzureProviderCmd = &cobra.Command{
 	Use:   "check-azure-provider",
 	Short: "Checks if the user has Azure cloud permissions",
 	Long:  "Checks if service principal has the correct permissions to use the azure cloud provider",
-	//Run:   checkAzureProviderRun,
+	Run:   checkAzureProviderRun,
 }
 
 func init() {
-	checkCmd.Flags().StringVar(&kind, "kind", "", "Cloud provider type")
+
 	checkGoogleProviderCmd.Flags().StringVarP(&cfg.GooglePath, "service-account-path", "p", "", "sets the service account path (required)")
 	checkGoogleProviderCmd.Flags().StringVarP(&cfg.GoogleProjectName, "project-name", "n", "", "sets the project name (required)")
 	checkGoogleProviderCmd.Flags().StringVarP(&cfg.GoogleServiceEmail, "service-account-email", "e", "", "sets the service account email (required)")
-	//cloudProviderCmd.AddCommand(checkGoogleProviderCmd)
+	cloudProviderCmd.AddCommand(checkGoogleProviderCmd)
 
 	checkAmazonProviderCmd.Flags().StringVarP(&cfg.AwsIamUsername, "iam-user", "i", "", "sets the iam user (required)")
 	checkAmazonProviderCmd.Flags().StringVarP(&cfg.AwsAccessKey, "access-key", "a", "", "sets the access key (required)")
 	checkAmazonProviderCmd.Flags().StringVarP(&cfg.AwsSecretKey, "secret-key", "s", "", "sets the secret key (required)")
 	checkAmazonProviderCmd.Flags().StringVarP(&cfg.AwsRegion, "region", "r", "", "sets the region (required)")
-	//cloudProviderCmd.AddCommand(checkAmazonProviderCmd)
+	cloudProviderCmd.AddCommand(checkAmazonProviderCmd)
 
 	checkAzureProviderCmd.Flags().StringVarP(&cfg.AzureTenant, "tenant-id", "t", "", "sets the tenant id (required)")
 	checkAzureProviderCmd.Flags().StringVarP(&cfg.AzureClient, "client-id", "c", "", "sets the client(applicaiton) id (required)")
 	checkAzureProviderCmd.Flags().StringVarP(&cfg.AzureSubscription, "subscription-id", "s", "", "sets the ssubscription id (required)")
 	checkAzureProviderCmd.Flags().StringVarP(&cfg.AzureSecret, "secret-key", "k", "", "sets the secret key (required)")
-	//cloudProviderCmd.AddCommand(checkAzureProviderCmd)
-	cloudProviderCmd.AddCommand(checkCmd)
+	cloudProviderCmd.AddCommand(checkAzureProviderCmd)
 }
 
-func checkCmdRun(cmd *cobra.Command, args []string) {
-	if kind == "amazon" {
-		checkAmazonProviderRun(checkAmazonProviderCmd)
-	} else if kind == "azure" {
-		checkAzureProviderRun(checkAzureProviderCmd)
-	} else if kind == "google" {
-		checkGoogleProviderRun(checkGoogleProviderCmd)
-	} else {
-		zap.S().Fatalf("Please make sure correct cloud provider is passed (amazon/azure/google)")
-	}
-}
-
-func checkGoogleProviderRun(cmd *cobra.Command) {
+func checkGoogleProviderRun(cmd *cobra.Command, args []string) {
 
 	var err error
 	if cmd.Flags().Changed("no-prompt") {
@@ -103,7 +81,7 @@ func checkGoogleProviderRun(cmd *cobra.Command) {
 
 }
 
-func checkAmazonProviderRun(cmd *cobra.Command) {
+func checkAmazonProviderRun(cmd *cobra.Command, args []string) {
 	var err error
 	if cmd.Flags().Changed("no-prompt") {
 		flagsNotSet := checkFlags(cmd)
@@ -122,7 +100,7 @@ func checkAmazonProviderRun(cmd *cobra.Command) {
 	}
 }
 
-func checkAzureProviderRun(cmd *cobra.Command) {
+func checkAzureProviderRun(cmd *cobra.Command, args []string) {
 
 	var err error
 	if cmd.Flags().Changed("no-prompt") {
