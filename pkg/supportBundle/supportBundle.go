e46ff726cf8a3b58c621e8ad8007743276ccd145
devidask27 <devidas@platform9.com>
2022-06-20 16:11:33 +0530

    logupload fix

--- pkg/supportBundle/supportBundle.go
+++ pkg/supportBundle/supportBundle.go
@@ -246,8 +246,15 @@ func GenSupportBundle(exec cmdexec.Executor, timestamp time.Time, isRemote bool)
 
 	} else {
 		// Generation of supportBundle in local host case.
-		_, errbundle := exec.RunWithStdout("bash", "-c", fmt.Sprintf("tar czf %s --directory=%s %s %s %s %s %s %s %s",
-			targetfile, util.Pf9DirLoc, util.Pf9LogLoc, util.VarDir, util.EtcDir, util.DmesgLog, msgfile, lockfile, util.OptDir))
+		var errbundle error
+		allCLILogfiles := util.LogFileNamePath[:len(util.LogFileNamePath)-4] + "*"
+		if util.Pf9LogLoc != util.DefaultPf9LogLoc {
+			_, errbundle = exec.RunWithStdout("bash", "-c", fmt.Sprintf("tar czf %s --directory=%s %s %s %s %s %s %s %s",
+				targetfile, util.Pf9DirLoc, allCLILogfiles, util.VarDir, util.EtcDir, util.DmesgLog, msgfile, lockfile, util.OptDir))
+		} else {
+			_, errbundle = exec.RunWithStdout("bash", "-c", fmt.Sprintf("tar czf %s --directory=%s %s %s %s %s %s %s %s",
+				targetfile, util.Pf9DirLoc, util.Pf9LogLoc, util.VarDir, util.EtcDir, util.DmesgLog, msgfile, lockfile, util.OptDir))
+		}
 		if errbundle != nil {
 			zap.S().Debug("Failed to generate complete supportBundle, generated partial bundle", errbundle)
 			return targetfile, ErrPartialBundle
