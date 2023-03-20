6fe69daf849cbc56e0dcd17be58a23e99ee89e16
devidask27 <devidas@platform9.com>
2023-03-20 17:18:19 +0530

    updating version to 1.21

--- pkg/util/constants.go
+++ pkg/util/constants.go
@@ -305,7 +305,7 @@ func init() {
 
 // These are the constants needed for everything version related
 const (
-	Version         string = "pf9ctl version: v1.20"
+	Version         string = "pf9ctl version: v1.21"
 	AWSBucketName   string = "pmkft-assets"
 	AWSBucketKey    string = "pf9ctl"
 	AWSBucketRegion string = "us-west-1"
