923a95de31d894a3e4c984b02c96ac2679fa60d0
joshivedant84 <joshivedant84@gmail.com>
2024-10-14 07:50:26 +0000

    Upate pf9ctl version

--- pkg/util/constants.go
+++ pkg/util/constants.go
@@ -306,7 +306,7 @@ func init() {
 
 // These are the constants needed for everything version related
 const (
-	Version         string = "pf9ctl version: v1.28"
+	Version         string = "pf9ctl version: v1.29"
 	AWSBucketName   string = "pmkft-assets"
 	AWSBucketKey    string = "pf9ctl"
 	AWSBucketRegion string = "us-west-1"
