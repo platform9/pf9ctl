b5713bb0373d92d47dad7737848742d5196502b1
Mridul Gain <mridulgain@gmail.com>
2025-02-28 18:12:49 +0530

    PMK-6388: fix issue with host_id extraction (#403)
    * fix issue with host_id extraction
    * unit tests addition
    
    ---------
    
    Signed-off-by: Mridul Gain <mridul@platform9.com>
    Co-authored-by: bito-code-review[bot] <188872107+bito-code-review[bot]@users.noreply.github.com>

--- pkg/pmk/node_test.go
+++ pkg/pmk/node_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/platform9/pf9ctl/pkg/client"
 	"github.com/platform9/pf9ctl/pkg/cmdexec"
+	"github.com/platform9/pf9ctl/pkg/keystone"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,6 +60,100 @@ func TestOpenOSReleaseFile(t *testing.T) {
 			result, err := OpenOSReleaseFile(tc.args.exec)
 			assert.Equal(t, tc.want.err, err)
 			assert.Equal(t, tc.want.result, result)
+		})
+	}
+}
+func TestGetHostIDFromConf(t *testing.T) {
+	type args struct {
+		exec cmdexec.Executor
+		auth keystone.KeystoneAuth
+	}
+	type want struct {
+		hostID string
+		err    error
+	}
+
+	cases := map[string]struct {
+		args
+		want
+	}{
+		"Success": {
+			args: args{
+				exec: &cmdexec.MockExecutor{
+					MockRunWithStdout: func(name string, args ...string) (string, error) {
+						return "host_id = xxxx-9688-46a2-aa4f-xxx", nil
+					},
+				},
+				auth: keystone.KeystoneAuth{},
+			},
+			want: want{
+				hostID: "xxxx-9688-46a2-aa4f-xxx",
+				err:    nil,
+			},
+		},
+		"ErrorGrep": {
+			args: args{
+				exec: &cmdexec.MockExecutor{
+					MockRunWithStdout: func(name string, args ...string) (string, error) {
+						return "", fmt.Errorf("stdout:stderr:grep: /etc/pf9/host_id.conf: No such file or directory")
+					},
+				},
+				auth: keystone.KeystoneAuth{},
+			},
+			want: want{
+				hostID: "",
+				err:    fmt.Errorf("error: unable to grep host_id: stdout:stderr:grep: /etc/pf9/host_id.conf: No such file or directory"),
+			},
+		},
+		"EmptyOutput": {
+			args: args{
+				exec: &cmdexec.MockExecutor{
+					MockRunWithStdout: func(name string, args ...string) (string, error) {
+						return "", nil
+					},
+				},
+				auth: keystone.KeystoneAuth{},
+			},
+			want: want{
+				hostID: "",
+				err:    fmt.Errorf("error: host_id not found in /etc/pf9/host_id.conf"),
+			},
+		},
+		"InvalidFormat": {
+			args: args{
+				exec: &cmdexec.MockExecutor{
+					MockRunWithStdout: func(name string, args ...string) (string, error) {
+						return "invalid_format", nil
+					},
+				},
+				auth: keystone.KeystoneAuth{},
+			},
+			want: want{
+				hostID: "",
+				err:    fmt.Errorf("error: host_id key=value pair not found in config"),
+			},
+		},
+		"NoValue": {
+			args: args{
+				exec: &cmdexec.MockExecutor{
+					MockRunWithStdout: func(name string, args ...string) (string, error) {
+						return "host_id = ", nil
+					},
+				},
+				auth: keystone.KeystoneAuth{},
+			},
+			want: want{
+				hostID: "",
+				err:    fmt.Errorf("error: no host_id value found in config"),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			hostID, err := getHostIDFromConf(client.Client{Executor: tc.args.exec, Segment: client.NewSegment("mgain.pf9.test", false)}, tc.args.auth)
+			assert.Equal(t, tc.want.err, err)
+			assert.Equal(t, tc.want.hostID, hostID)
 		})
 	}
 }
