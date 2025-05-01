bcfeec49900ba309fe74e9253a8a0655bca5caef
snslk <snehal@platform9.com>
2025-05-01 18:02:16 +0530

    update to 1.32

--- pkg/util/constants.go
+++ pkg/util/constants.go
@@ -323,7 +323,7 @@ func init() {
 
 // These are the constants needed for everything version related
 const (
-	Version         string = "pf9ctl version: v1.31"
+	Version         string = "pf9ctl version: v1.32"
 	AWSBucketName   string = "pmkft-assets"
 	AWSBucketKey    string = "pf9ctl"
 	AWSBucketRegion string = "us-west-1"
